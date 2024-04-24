0764e1effaaf671daeb33476ffeee34a5bc9231a
jzzjy0822 <jzzjy0822@gmail.com>
2024-04-24 06:01:53 +0000

    fix the limbo update issue

--- qplacer/compile.sh
+++ qplacer/compile.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 set -euo pipefail
-DEST_DIR=$(pwd)
 
+DEST_DIR=$(pwd)
 BUILD_DIR="../build"
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir -p "$BUILD_DIR"
 fi
+
 cd "$BUILD_DIR"
 
-cmake .. -DCMAKE_INSTALL_PREFIX=${DEST_DIR}/operators
+if [ $# -eq 0 ]; then
+    echo "No INSTALL_BACKUP option provided, defaulting to ON"
+    INSTALL_BACKUP="ON"
+else
+    INSTALL_BACKUP=$1
+fi
+
+cmake .. -DCMAKE_INSTALL_PREFIX="${DEST_DIR}/operators" -DINSTALL_BACKUP=${INSTALL_BACKUP}
 make
 make install
-cd ${DEST_DIR}
+
+cd "${DEST_DIR}"
